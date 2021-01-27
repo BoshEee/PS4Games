7f5f92cb68a0c7192be4ce1c94c948e4845c5d2a
BoshEee <bosheee.shwi@gmail.com>
2021-01-27 09:59:05 +0200

    creating the server

--- router.js
+++ router.js
@@ -1,1 +1,16 @@
+const homeHandler = require("./Handlers/home");
+const missingHandler = require("./Handlers/missing");
+const publicHandler = require("./Handlers/public");
 
+function router( request, response) {
+    const url = request.url;
+    if ( url === "/") {
+        homeHandler(request, response);
+    } else if (url.includes("public")) {
+        publicHandler(request, response);
+    } else {
+        missingHandler(request, response);
+    }
+}
+
+module.exports = router;
--- test.js
+++ test.js
@@ -1,1 +1,25 @@
+const router = require("./router");
+const homeHandler = require("./Handlers/home");
+
+const test = require("tape");
+const supertest = require("supertest");
+
+test("Initialise", t => {
+    let num = 2;
+    t.equal(num, 2, "should return 2");
+    t.end();
+});
+
+test("Home route returns status code 200", t => {
+    supertest(router)
+    .get("/")
+    .expect(200)
+    .end((err, res) => {
+        t.error(err);
+        t.equal(res.status, 200);
+        t.end();
+    });
+});
+
+
 
