e1d271fe1dd758301485ee8eddd23c770d2ba7e1
BoshEee <bosheee.shwi@gmail.com>
2021-01-28 12:42:27 +0200

    adding datalist

--- public/index.js
+++ public/index.js
@@ -1,20 +1,27 @@
 const searchInput = document.querySelector("input");
+const dataList = document.getElementById("nameResults");
 
 searchInput.addEventListener("keyup", (event) => {
-  console.log(searchInput.value);
+  console.log("Input : " + searchInput.value);
 
   fetch(`/ps4?name=${searchInput.value}`)
     .then((response) => {
       if (!response.ok) throw new Error(response.status);
-      return response.json()
+      return response.json();
     })
     .then((result) => {
       let res = JSON.parse(result);
       let games = res.games.filter((game) => {
+        return game.name
+          .toLowerCase()
+          .startsWith(searchInput.value.toLowerCase());
+      });
 
-        return game.name.toLowerCase().startsWith(searchInput.value.toLowerCase());
-      })
-      console.log(games);
+      for (let i = 0; i <= games.length - 1; i++) {
+        let opt = "";
+        opt = '<option value="'+games[i].name+'" />';
+        dataList.innerHTML = opt;
+      }
     })
     .catch((error) => {
       console.log(error);
