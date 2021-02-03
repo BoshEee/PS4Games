2beee6ad752121c0cc30c3aecf5911a2ff703670
MayDarawshi <May.Darawshi19@gmail.com>
2021-02-03 11:41:57 +0200

    gameInfo

---
+++ Handlers/gameInfo.js
@@ -0,0 +1,12 @@
+// const fs = require("fs");
+// const path = require("path");
+
+// function gameInfoHandler(request, response) {
+//     const filePath = path.join(__dirname, "..", "public", "gameInfo.html");
+//     fs.readFile(filePath, (err, file) => {
+//         response.writeHead(200, { "content-type": "text/html" });
+//         response.end(file); // file?
+//     });
+// }
+
+// module.exports = gameInfoHandler;
