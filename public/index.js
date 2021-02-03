3f0077869567264949e508cf1367259d986c4d33
Jihad Banna <super_banna@hotmail.com>
2021-02-03 14:23:24 +0200

    display

--- public/index.js
+++ public/index.js
@@ -6,6 +6,7 @@ const info = document.querySelector(".info");
 const title = document.querySelector("#title");
 const genres = document.querySelector("#genres");
 const poster = document.querySelector("#poster");
+const myForm = document.querySelector("form");
 
 
 
@@ -17,13 +18,22 @@ searchInput.addEventListener("keyup", (event) => {
             return response.json();
         })
         .then((result) => {
+            console.log(result)
 
-            for (let i = 0; i <= result.length - 1; i++) {
+            if (result.length > 1) {
+                for (let i = 0; i <= result.length - 1; i++) {
+                    const opt = document.createElement('option')
+                    opt.innerHTML = `<option value="${result[i].name}" />`;
+                    dataList.appendChild(opt)
+
+                }
+            } else {
                 const opt = document.createElement('option')
-                opt.innerHTML = `<option value="${result[i].name}" />`;
+                opt.innerHTML = `<option value="${result.name}" />`;
                 dataList.appendChild(opt)
 
             }
+
         })
         .catch((error) => {
             console.log(error);
@@ -47,11 +57,17 @@ function getinfo(event) {
                 return response.json();
             })
             .then((result) => {
-                info.style.display = "block";
-                title.textContent = result[0].name;
-                genres.textContent = "Genres : " + result[0].genres;
-                poster.src = result[0].images;
-                console.log("name : " + result[0].genres);
+                console.log(result);
+                if (result.length == 0) {
+                    alert("No Results");
+                } else {
+                    myForm.style.display = "none";
+                    info.style.display = "block";
+                    title.textContent = result[0].name;
+                    genres.textContent = "Genres : " + result[0].genres;
+                    poster.src = result[0].images;
+                    console.log("name : " + result[0].genres);
+                }
             })
             .catch((error) => {
                 console.log(error);
